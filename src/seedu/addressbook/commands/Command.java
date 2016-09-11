b874d24c515337f78f77c8643c33d880f31dade3
ljsjack <lowjiansheng@hotmail.sg>
2016-09-11 13:40:04 +0800

    Add throw Exception to child execute() method.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,7 +40,7 @@ public abstract class Command {
      * 
      * @return true if the command changes the value of the data
      */
-    public abstract boolean isMutating();
+    public abstract boolean isMutating() throws Exception;
     
     /**
      * Executes the command and returns the result.
